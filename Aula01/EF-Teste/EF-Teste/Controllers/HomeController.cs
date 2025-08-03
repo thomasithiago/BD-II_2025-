7c63befa679e6585c44b2e14029475a3deffc268
Thiago Thomasi <156266186+thomasithiago@users.noreply.github.com>
2025-08-02 21:18:57 -0300

    Aula inicial BD

---
+++ Aula01/EF-Teste/EF-Teste/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using EF_Teste.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EF_Teste.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
